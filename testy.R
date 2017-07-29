c09be3be9234c24610e64052bdd4d7120bc9c008
John Davis <davisjf@gmail.com>
2017-07-29 09:10:09 -0400

    adding some additional code before i add a readme

--- testy.R
+++ testy.R
@@ -2,5 +2,15 @@
 #
 # to install the devtools package do on console 
 # install.packages("devtools")
+#
+# Sadly this will install httr, which I was trying to ensure that my dependency
+# was correct in my package.
 library(devtools)
-install_github("netskink", "CSJFD")
+install_github("netskink/CSJFD")
+library(CSJFD)
+hello()
+#myTest()
+#remove.packages("CSJFD")
+# these should fail
+#hello()
+#myTest()
